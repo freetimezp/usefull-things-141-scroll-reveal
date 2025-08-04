1c8bdc44b07bdb08b8580def30afc5320d6246e7
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-08-04 19:19:33 +0300

    04. usefull-things-141-scroll-reveal. Bg, images.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,39 @@
-
 gsap.registerPlugin(ScrollTrigger);
 
-const bgColors = [
-    "#faba4a",
-    "#bb2a26",
-    "#7e7d65",
-    "#989682",
-    "#5e4036",
-    "#e33b12",
-    "#252d1a",
-    "#b04c0d"
-];
+const bgColors = ["#e14434", "#1378bb", "#eb872f", "#095741", "#6643e5", "#d36c61", "#0b55a0", "#d2900c"];
 const bgColorElement = document.querySelector(".bg-color");
 
 gsap.utils.toArray(".item").forEach((item, index) => {
     let img = item.querySelector(".item-img img");
 
-    gsap.fromTo(img, {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)"
-    }, {
-        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
-        ease: "power1.out",
-        duration: 2,
-        scrollTrigger: {
-            trigger: item,
-            start: "center bottom",
-            end: "bottom top",
-            toggleActions: "play none none none",
-            onEnter: () => updateBackground(bgColors[index]),
-            onEnterBack: () => updateBackground(bgColors[index]),
+    gsap.fromTo(
+        img,
+        {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)",
+            scale: 1,
+        },
+        {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            ease: "power1.out",
+            scale: 1.25,
+            duration: 2,
+            scrollTrigger: {
+                trigger: item,
+                start: "center bottom",
+                end: "bottom top",
+                toggleActions: "play reverse play reverse",
+                onEnter: () => updateBackground(bgColors[index]),
+                onEnterBack: () => updateBackground(bgColors[index]),
+            },
         }
-    });
+    );
 });
 
 function updateBackground(color) {
     gsap.to(bgColorElement, {
-        background: `linear-gradient(0deg, ${color} 0%, rgba(252, 176, 69, 0) 100%)`,
+        background: `linear-gradient(0deg, ${color} 100%, rgba(252, 176, 69, 0) 100%)`,
         duration: 2,
-        ease: "power1.out"
+        ease: "power1.out",
     });
 }
 
@@ -53,23 +49,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
     window.addEventListener("scroll", updateScrollPercentage);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
